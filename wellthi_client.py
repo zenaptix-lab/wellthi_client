4cad8666c4c1cd5bbef7697d4502c0cd307467cb
Rikus de Milander <rickmmmbra@gmail.com>
2018-11-01 13:56:49 +0200

    added basic json parsing and route handeling.

--- wellthi_client.py
+++ wellthi_client.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+print("Starting web server")
+
+@app.route('/')
+def helloWorld():
+    return "Hello world!!!"
+
+@app.route('/hello', methods=['GET','POST'])
+def helloPost():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        email = json_data['email']
+        password = json_data['password']
+        return jsonify(email, password)
+    else:
+        return jsonify("{'hello' : 'world'}")
+
+
+
+if __name__ == '__main__':
+    app.run()
