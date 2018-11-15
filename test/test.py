bb292a896b62715986dad98580267d6f433b5aa8
Rikus de Milander <rickmmmbra@gmail.com>
2018-11-15 17:24:04 +0200

    trying to get user input from chat bot logs using the correct entity.

--- test/test.py
+++ test/test.py
@@ -17,42 +17,41 @@ if __name__ == '__main__':
     #
     # print(json.dumps(response, indent=2))
 
-    chat_server = MessageHelpers("d01d68b2-3864-4401-a26d-92b10ef74e48","FUWYZmMJmjGF",'2018-09-20')
+    chat_server = MessageHelpers("d01d68b2-3864-4401-a26d-92b10ef74e48", "FUWYZmMJmjGF", '2018-09-20')
 
     ############## chat interaction 1
-    print("chat_context BEFORE POST : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"stressed")
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context BEFORE POST : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "stressed")
+    print("chat_context : ", chat_server.chat_context)
     print(response)
 
     ############## chat interaction 2
-    print("chat_context BEFORE POST2 : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"Y")
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context BEFORE POST2 : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "Y")
+    print("chat_context : ", chat_server.chat_context)
     print(response)
 
     ############## chat interaction 3
-    print("chat_context BEFORE POST3 : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"Y")
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context BEFORE POST3 : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "Y")
+    print("chat_context : ", chat_server.chat_context)
     print(response)
 
     ############## chat interaction 4
-    print("chat_context BEFORE POST4 : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"3")
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context BEFORE POST4 : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "3")
+    print("chat_context : ", chat_server.chat_context)
     print(response)
 
     ############## chat interaction 5
-    print("chat_context BEFORE POST5 : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"angry")
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context BEFORE POST5 : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "angry")
+    print("chat_context : ", chat_server.chat_context)
     print(response)
 
-
     ############## chat interaction 6
-    print("chat_context BEFORE POST6 : " , chat_server.chat_context)
-    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d',"9")
+    print("chat_context BEFORE POST6 : ", chat_server.chat_context)
+    response = chat_server.post_message('953d25b4-9170-47e5-b465-fc513f60ce1d', "9")
     end = chat_server.chat_context['system']
 
     if 'branch_exited_reason' in end:
@@ -60,5 +59,18 @@ if __name__ == '__main__':
     else:
         print("########################################## I am still chatting")
 
-    print("chat_context : " , chat_server.chat_context)
+    print("chat_context : ", chat_server.chat_context)
     print(response)
+
+    watson_entities = chat_server.assistant.list_entities('953d25b4-9170-47e5-b465-fc513f60ce1d').get_result()
+    print(watson_entities)
+    # print(json.dumps(watson_entities,indent=2))
+    for entity in watson_entities['entities']:
+        entity_ref = entity['entity']
+        print(entity_ref)
+
+        entity_info = chat_server.assistant.get_entity('953d25b4-9170-47e5-b465-fc513f60ce1d', entity_ref)
+        # print(entity_info)
+
+    log_events = chat_server.assistant.list_logs('953d25b4-9170-47e5-b465-fc513f60ce1d', request_timestamp="2018-11-14T00:00:00.000Z")
+    print(log_events)
